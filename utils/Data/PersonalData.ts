a1a8ff602fe0f2634885c6f418bad58178f313a6
issakobayashi1992 <orurozil266@gmail.com>
2026-04-17 13:20:16 -0700

    ADD: added initial commit for my portfolio

---
+++ utils/Data/PersonalData.ts
@@ -0,0 +1,27 @@
+export const personalData = {
+  name: "Sasaki Kaito",
+  profile: "/me.png",
+  designation: "Full-Stack Web",
+  designationAlternateWords: [
+    "Developer.",
+    "Engineer.",
+    "Architect.",
+    "Innovator.",
+    "Designer.",
+  ],
+  description:
+    "I’m Sasaki Kaito, a dedicated Full-Stack Developer specializing in TypeScript and crafting responsive, user‑centric interfaces with React and Next.js. I build full‑stack websites using Next.js and Prisma—leveraging Prisma’s type‑safe ORM to integrate robust database layers seamlessly into my applications. Passionate about continuous learning, I stay at the forefront of emerging technologies to deliver scalable, maintainable solutions that exceed expectations.",
+  email: "kaitosasaki725@gmail.com",
+  phone: "+813175972979",
+  address: "Osaka",
+  github: "https://github.com/DevNucleus86",
+  facebook: "https://www.facebook.com/profile.php?id=100092595303195",
+  Instagram: "https://www.instagram.com/captain_ghost005",
+  linkedIn: "https://www.linkedin.com/in/abdulbasit005/",
+  twitter: "https://twitter.com/Ghost_oo5",
+  stackOverflow: "https://stackoverflow.com/users/15349472/captain-ghost",
+  devUsername: "GHOST 005",
+  leetcode: "https://leetcode.com/u/DevNucleus86/",  
+  resume:
+    "https://drive.google.com/file/d/1TmtDCIlCaZJ4b0npzjwsdDtxiOWwye23/view?usp=drive_link",
+};
